fd670a262e378cfa172d88c23718aea2b9f306f2
Radosław Biesek <radoslaw.biesek@gmail.com>
2022-07-24 16:40:20 +0200

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,8 @@ module.exports = {
     'no-param-reassign': 'off',
     camelcase: 'off',
     'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'warn',
   },
 };
